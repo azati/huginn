5207aced1637e8e7791a836ea4d978b8d9e2f6f8
Ivan Buiko <ivan@buiko.com>
2015-10-10 00:05:02 +0300

    Validate beeper agent options

--- app/models/agents/beeper_agent.rb
+++ app/models/agents/beeper_agent.rb
@@ -6,8 +6,6 @@ module Agents
 
     API_BASE = 'https://api.beeper.io/api'.freeze
 
-    MESSAGE_TYPES = %w(message image event location task).freeze
-
     TYPE_TO_ATTRIBUTES = {
       'message'  => %w(text),
       'image'    => %w(text image),
@@ -16,21 +14,53 @@ module Agents
       'task'     => %w(text)
     }.freeze
 
+    MESSAGE_TYPES = TYPE_TO_ATTRIBUTES.keys
+
+    TYPE_REQUIRED_ATTRIBUTES = {
+      'message'  => %w(text),
+      'image'    => %w(image),
+      'event'    => %w(text start_time),
+      'location' => %w(latitude longitude),
+      'task'     => %w(text)
+    }.freeze
+
     def default_options
       {
         'type'       => 'message',
-        'app_id'     => '',
-        'api_key'    => '',
+        'app_id'     => '<BEEPER_APPLICATION_ID>',
+        'api_key'    => '<BEEPER_REST_API_KEY>',
         'sender_id'  => '',
         'phone'      => '',
+        'group_id'   => '',
 
         'text'       => '{{title}}',
-        'image'      => '{{url}}',
-        'start_time' => '',
-        'end_time'   => '',
-        'latitude'   => '',
-        'longitude'  => ''
+        # TODO: discuss
+        # 'image'      => '{{url}}',
+        # 'start_time' => '',
+        # 'end_time'   => '',
+        # 'latitude'   => '',
+        # 'longitude'  => ''
       }
+    end
+
+    def validate_options
+      %w(app_id api_key sender_id type).each do |attr|
+        errors.add(:base, "you need to specify a #{attr}") if options[attr].blank?
+      end
+
+      if !options['type'].in?(MESSAGE_TYPES)
+        errors.add(:base, 'you need to specify a valid message type')
+      else
+        required_attributes = TYPE_REQUIRED_ATTRIBUTES[options['type']]
+        if required_attributes.any? {|attr| options[attr].blank? }
+          errors.add(:base, "you need to specify a #{required_attributes.join(', ')}")
+        end
+      end
+
+
+      unless options['group_id'].blank? ^ options['phone'].blank?
+        errors.add(:base, "you need to specify a phone or group_id") # TODO: Change text
+      end
     end
 
     def working?
